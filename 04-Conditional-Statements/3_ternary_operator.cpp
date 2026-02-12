84db6aad741854867aff9f3786da4f7f4df764a5
hanifa-khanam <bc250220499bha@vu.edu.pk>
2026-02-12 13:53:37 +0500

    Add new C++ conditional statements examples including if-else, else-if, ternary operator, switch statement, and assignment questions

--- 04-Conditional-Statements/3_ternary_operator.cpp
+++ 04-Conditional-Statements/3_ternary_operator.cpp
@@ -5,8 +5,9 @@ int main() {
 
     // Largest of 2 numbers
     int a = 10;
-    int b = 5;
+    int b = 59;
 
+    // syntax: variable = condition? statement1 : statement2;
     int largest = a >= b? a : b;
     cout << "largest is : " << largest << endl;
     
